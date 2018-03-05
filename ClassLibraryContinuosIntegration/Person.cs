75edf471ba4ebb126d0c55175213dec3384da665
wandriey <jona434g@edu.easj.dk>
2018-03-05 12:10:35 +0100

    Test projekt er tilføjet
    tester om getname metode returnere mit navn.

--- ClassLibraryContinuosIntegration/Person.cs
+++ ClassLibraryContinuosIntegration/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibraryContinuosIntegration
 {
-    class Person
+    public class Person
     {
         public string GetName()
         {
---
+++ ClassLibraryContinuosIntegrationTests/PersonTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryContinuosIntegration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryContinuosIntegration.Tests
+{
+    [TestClass()]
+    public class PersonTests
+    {
+        [TestMethod()]
+        public void GetNameTest()
+        {
+            //arrange 
+            Person person1 = new Person();
+            //act
+            string name = person1.GetName();
+            //assert
+            Assert.AreEqual(name, "Jonas");
+        }
+    }
+}
